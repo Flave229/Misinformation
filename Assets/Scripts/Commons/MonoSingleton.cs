9613645111b23f69c019be9003547d5243ac3050
PARKHOUSE-PEMBERTON Ashley T <p016671e@student.staffs.ac.uk>
2018-01-30 11:27:20 +0000

    Adding Commons

---
+++ Assets/Scripts/Commons/MonoSingleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance {get; private set; }
+
+    public static IEnumerator WaitOnInstance()
+    {
+        while (Instance == null)
+        {
+            yield return null;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+		//Debug.LogWarning("MonoSingleton Instance Set!" + gameObject.name);
+        Instance = this as T;
+    }
+}

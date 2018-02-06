f750d11b069149193fcdb84bd01ccfb960a3a13e
WALTERS Ray J <w018405b@student.staffs.ac.uk>
2018-02-06 11:31:52 +0000

    Displaying text from converse data i.e. the dialogue or speech wraps appropriately and there is a bigger delay between conversations, reordered the layers so the daily report can be submitted and made my text in the panel inactive like it was originally - missed this on initial commit.

--- Assets/Scripts/AI/Tasks/ConverseTask.cs
+++ Assets/Scripts/AI/Tasks/ConverseTask.cs
@@ -15,8 +15,7 @@ namespace Assets.Scripts.AI.Tasks
         private float _timeTalking;
         private readonly float _defaultTimeWaiting;
         private float _timeWaiting;
-        private GameObject _speechBubble; //private is needed this is for testing
-        //private GameObject _speechText;
+        private GameObject _speechBubble;
 
         public ConverseTask(ConverseData converseData)
         {
@@ -27,9 +26,6 @@ namespace Assets.Scripts.AI.Tasks
             _converseData = converseData;
             _conversationManager = ConversationManager.Instance();
             _speechBubble = GameObject.FindGameObjectWithTag("ConversationPanel");
-            // _speechBubble = null; // will be changed since using tags is inefficient but it will be used like this for now to make it work - R.Walters
-            // _speechText = null;
-            //_speechBubble.SetActive(false);
         }
 
         public void Execute()
@@ -55,39 +51,15 @@ namespace Assets.Scripts.AI.Tasks
 
             if (_converseData.Talking == false && _converseData.Done == false && _converseData.ConversationPartnerTaskData.Talking == false)
             {
-               // Color colorSpeechBubble = _speechBubble.GetComponent<Image>().material.color;
-            //    Color colorSpeechBubbleName = _speechBubble.transform.Find("TextName").GetComponent<Text>().material.color;
-            //    Color colorSpeechBubbleDialogue = _speechBubble.transform.Find("Dialogue").GetComponent<Text>().material.color;
-             //   colorSpeechBubbleDialogue = new Color(colorSpeechBubbleDialogue.r, colorSpeechBubbleDialogue.g, colorSpeechBubbleDialogue.b, 100.0f);
-             //   colorSpeechBubble = new Color(colorSpeechBubble.r, colorSpeechBubble.g, colorSpeechBubble.b, 100.0f);
-             //   colorSpeechBubbleName = new Color(colorSpeechBubbleName.r, colorSpeechBubbleName.g, colorSpeechBubbleName.b, 100.0f);
-              //  _speechBubble.GetComponent<Image>().material.color = colorSpeechBubble;
-              //  _speechBubble.transform.Find("TextName").GetComponent<Text>().material.color = colorSpeechBubbleName;
-              //  _speechBubble.transform.Find("Dialogue").GetComponent<Text>().material.color = colorSpeechBubbleDialogue;
-                // GameObject.Find("GOConversation").transform.Find("CanvasConversation").gameObject.SetActive(true);
-                // _speechBubble = GameObject.FindGameObjectWithTag("ConversationPanel");
-                //GameObject.Find("GOConversation").SetActive(true);
                 _converseData.Talking = true;
                 var conversation =  _conversationManager.ConversationGenerator(_converseData.General, _converseData.ConversationPartnerTaskData.General);
                 Debug.Log("A conversation started!");
 				SoundManager.Instance ().PlaySingleDistance (_converseData.General.gameObject, "generalConversation1", 1.0f, 10.0f);
 				SoundManager.Instance ().PlaySingleDistance (_converseData.ConversationPartnerTaskData.General.gameObject, "generalConversation2", 1.0f, 10.0f);
                 _converseData.Speech = conversation[_converseData.General];
-                _converseData.ConversationPartnerTaskData.Speech = conversation[_converseData.ConversationPartnerTaskData.General];
-                //_speechBubble = GameObject.Instantiate(Resources.Load<GameObject>("SpeechBubble"));
-                 // _speechText = GameObject.Instantiate(new GameObject("SpeechText"));
-               // _speechText.AddComponent<TextMesh>();
-               //  _speechText.GetComponent<TextMesh>().text = _speechBubble.GetComponent<Text>().text;
-               // _speechBubble.AddComponent<TextMesh>();
-              //  _speechBubble.transform.parent = GameObject.Find("CanvasConversation").transform;
-                //_speechBubble.
+                _converseData.ConversationPartnerTaskData.Speech = conversation[_converseData.ConversationPartnerTaskData.General];                
                 _speechBubble.transform.Find("Dialogue").GetComponent<Text>().text = _converseData.Speech;
                 _speechBubble.transform.Find("TextName").GetComponent<Text>().text = _converseData.General.Name.FullName();
-                //    _speechText.transform.parent = GameObject.Find(_speechBubble.name).transform;
-                //  _speechBubble.transform.position = new Vector3(_converseData.General.transform.position.x - 0.4f, _converseData.General.transform.position.y + 1.0f, 0.0f);
-                //  // _speechText.transform.position = _speechBubble.transform.position;
-                // _speechBubble.SetActive(true);
-                //   _speechText.SetActive(true);
                 _speechBubble.transform.Find("TextName").gameObject.SetActive(true);
                 _speechBubble.transform.Find("Dialogue").gameObject.SetActive(true);
             }
@@ -113,9 +85,6 @@ namespace Assets.Scripts.AI.Tasks
 
         public void SetCompleted()
         {
-            //if(_speechBubble != null)
-               // Object.Destroy(_speechBubble);
-
             _completed = true;
         }
 

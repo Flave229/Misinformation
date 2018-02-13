418e9561f5f0c2d458e8b931ad3f3170c45c1e3f
Kyle Twist <kyletwist@hotmail.co.uk>
2018-02-13 13:56:10 +0000

    Stopped generals going into the tech room

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -7,12 +7,11 @@ namespace Assets.Scripts
         public int  m_Depth;
         public Door m_DoorDown;
         public Door m_DoorUp;
-        public string m_RoomName;
+        public bool m_Accessible = true;
 
         // Use this for initialization
         void Start ()
         {
-		
         }
 	
         // Update is called once per frame

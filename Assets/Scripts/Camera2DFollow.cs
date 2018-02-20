38eec7a9c511e400b81ddcd89536d5c2462e52bb
URRY Tristan D <u015860e@student.staffs.ac.uk>
2018-02-20 11:56:11 +0000

    listening devices no longer give exception when they decay

--- Assets/Scripts/Camera2DFollow.cs
+++ Assets/Scripts/Camera2DFollow.cs
@@ -52,26 +52,29 @@ namespace Assets.Scripts
 			}
             if (m_Active)
             {
-                // only update lookahead pos if accelerating or changed direction
-                float xMoveDelta = (target.position - m_LastTargetPosition).x;
-
-                bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
-
-                if (updateLookAheadTarget)
+                if(target != null)
                 {
-                    m_LookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+                    // only update lookahead pos if accelerating or changed direction
+                    float xMoveDelta = (target.position - m_LastTargetPosition).x;
+
+                    bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+                    if (updateLookAheadTarget)
+                    {
+                        m_LookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+                    }
+                    else
+                    {
+                        m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+                    }
+
+                    Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward * m_OffsetZ;
+                    Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+
+                    transform.position = newPos;
+
+                    m_LastTargetPosition = target.position;
                 }
-                else
-                {
-                    m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
-                }
-
-                Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward * m_OffsetZ;
-                Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
-
-                transform.position = newPos;
-
-                m_LastTargetPosition = target.position;
             }
 			else
 			{

baf4104a5a96dc401eedad31cf5b4d2c1d37db0b
LAWTON Jacob C <l010423c@student.staffs.ac.uk>
2018-02-27 15:10:44 +0000

    Start of UI task queue

---
+++ Assets/Scripts/Task Stack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskStack : MonoBehaviour
+{
+
+    //public Stack<GameObject> taskStack;
+    public Queue<GameObject> taskQueue;
+
+	void Start ()
+    {
+        taskQueue = new Queue<GameObject>();
+	}
+
+	void Update ()
+    {
+
+	}
+
+    public void PopTask()
+    {
+        taskQueue.Dequeue();
+    }
+
+    public void PushTask()
+    {
+        //taskQueue.Enqueue(//addHere Needs to inherit);
+    } 
+}
